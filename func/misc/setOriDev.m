8fd296eb58accaa2cbfcc0bf2d42a3df9e33d07e
edwinofsakh <edwinofsakh@gmail.com>
2014-09-23 10:48:22 +0400

    Prepare release
    Return versrion.txt

--- func/misc/setOriDev.m
+++ func/misc/setOriDev.m
@@ -5,7 +5,7 @@ ss = get(o0, 'SS');
 
 r0 = rotation(o0);
 a0 = Euler(r0);
-ad = dev*rand(length(o0),3);
+ad = dev*(0.5-rand(length(o0),3));
 a1 = a0 + ad;
 o1 = orientation('Euler', a1(:,1), a1(:,2), a1(:,3), cs, ss);
         
