3f07ac0f6ed03186e350e4fe1ad0f2ed9f46fe4b
edwinofsakh <edwinofsakh@gmail.com>
2015-03-09 22:49:31 +0300

    Update EBSDAM_settings.m

--- EBSDAM_settings.m
+++ EBSDAM_settings.m
@@ -1,27 +1,12 @@
-PCname = getComputerName();
-switch PCname
-    case 'officemse'
-        setpref('ebsdam','data_dir','E:\Sergey\TextureStudy\data\ebsd');
-        setpref('ebsdam','output_dir','E:\Sergey\TextureStudy\out\ebsdam');
-        setpref('ebsdam','cache_dir','E:\Sergey\TextureStudy\out\ebsdam\cache');
-        setpref('ebsdam','doc_dir','E:\Sergey\TextureStudy\result\ebsd\doc');
-    case 'tania-w7'
-        setpref('ebsdam','data_dir','E:\MatLab\EBSDAM\data');
-        setpref('ebsdam','output_dir','E:\MatLab\EBSDAM\res0001\out');
-        setpref('ebsdam','cache_dir','E:\MatLab\EBSDAM\res0001\cache');
-        setpref('ebsdam','doc_dir','E:\MatLab\EBSDAM\res0001\doc');
-    case 'edwin-inspiron'
-        setpref('ebsdam','data_dir','D:\Projects\TextureStudy\EBSD\data');
-        setpref('ebsdam','output_dir','D:\Projects\TextureStudy\EBSD\res0001\out');
-        setpref('ebsdam','cache_dir','D:\Projects\TextureStudy\EBSD\res0001\cache');
-        setpref('ebsdam','doc_dir','D:\Projects\TextureStudy\EBSD\res0001\doc');
-    case 'edwin-w7'
-        setpref('ebsdam','data_dir','F:\Projects\TextureStudy\EBSD\data');
-        setpref('ebsdam','output_dir','F:\Projects\TextureStudy\EBSD\res0001\out');
-        setpref('ebsdam','cache_dir','F:\Projects\TextureStudy\EBSD\res0001\cache');
-        setpref('ebsdam','doc_dir','F:\Projects\TextureStudy\EBSD\res0001\doc');
-    otherwise
-        disp('Setup directories!');
+if (ispref('ebsdam','isdirs') && getpref('ebsdam','isdirs') == 1)
+    setpref('ebsdam','data_dir','path\to\data\');
+    setpref('ebsdam','output_dir','path\to\output\');
+    setpref('ebsdam','cache_dir','path\to\cache\');
+    setpref('ebsdam','doc_dir','path\to\doc\');
+    % after setup type and evaluate:
+    % getpref('ebsdam','isdirs',1)
+else
+    disp('Setup directories!');
 end
 
 setpref('ebsdam','caching', 1);
